88f07d7226a897d77e99397f2654298ca920c699
kevskizhang <kevin.yiyu.zhang@gmail.com>
2025-11-07 01:05:44 -0600

    changed file name to bypass cdn caching

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -3,7 +3,7 @@ const Resume = () => {
     <div className="w-full h-screen flex flex-col items-center">
       <div className="w-full max-w-4xl flex-grow">
         <iframe
-          src="public/resume/resume.pdf"
+          src="public/resume-2025.pdf"
           title="Kevin Zhang's Resume"
           className="w-full h-full border-none"
         />
